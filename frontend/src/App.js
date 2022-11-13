7edf5199969cb430d0aca38db3d816a874f8373f
Ujawal Tiwari <ujawal0011@gmail.com>
2022-11-13 08:49:29 +0530

    This is the MERN portfolio website
    backend and frontend both are here

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Login } from "@mui/icons-material";
+
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Login from "./components/Login/Login";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getUser, loadUser } from "./actions/user";
+// import AdminPanel from "./components/Admin/AdminPanel";
+// import Timeline from "./components/Admin/Timeline";
+// import Youtube from "./components/Admin/Youtube";
+// import Project from "./components/Admin/Project";
+// import Loader from "./components/Loader/Loader";
+
+function App() {
+
+  return (
+     
+      <Router>
+        <Header />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Contact" element ={<Contact />} />
+            <Route path="/account" element ={<Login />} />
+         </Routes>
+         <Footer />
+      </Router>
+
+  )
+}
+
+  export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import { Provider } from "react-redux";
+// import store from "./Store";
+// import { Provider as AlertProvider, positions, transitions } from "react-alert";
+// import AlertTemplate from "react-alert-template-basic";
+
+ReactDOM.render(
+  <React.StrictMode>
+        <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
